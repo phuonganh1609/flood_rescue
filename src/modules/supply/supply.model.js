d9f0ba93b6d67bf04b49ed75698cc15cc00b5763
phuonganh1609 <164039087+phuonganh1609@users.noreply.github.com>
2026-02-23 08:30:38 +0700

    Feat: add supply model

---
+++ src/modules/supply/supply.model.js
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+const SupplySchema = new Schema(
+  {
+    name: {
+         type: String, 
+         unique: true,
+         required: true 
+    },
+    category: { 
+        type: String, 
+        enum: ['FOOD', 'WATER', 'MEDICAL', 'CLOTHING', 'EQUIPMENT', 'OTHER'],
+        required: true 
+    },
+    unit: { 
+        type: String, 
+        required: true 
+    },
+    unitWeight: { 
+        type: Number, 
+        required: true 
+    },
+    description: { 
+        type: String, 
+        required: true 
+    },
+    isActive: { 
+        type: Boolean, 
+        default: true 
+    },
+    createdAt: { 
+        type: Date, 
+        default: Date.now 
+    },
+    updatedAt: { 
+        type: Date, 
+        default: Date.now 
+    },
+  },
+  { timestamps: true },
+);
+export default mongoose.model("Supply", SupplySchema);
